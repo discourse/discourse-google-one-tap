822b4a5e58a2fd97bd260b712e65c7e610371919
Jarek Radosz <jradosz@gmail.com>
2023-12-13 13:00:34 +0100

    DEV: Update linting

--- spec/requests/google_one_tap_tags_spec.rb
+++ spec/requests/google_one_tap_tags_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe "Google One Tap div and script tags" do
   before { SiteSetting.has_login_hint = false }
--- test/javascripts/acceptance/google-one-tap-test.js
+++ test/javascripts/acceptance/google-one-tap-test.js
@@ -1,7 +1,6 @@
-import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
-
 import { test } from "qunit";
+import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("Google One Tap when disabled", function (needs) {
   needs.settings({
